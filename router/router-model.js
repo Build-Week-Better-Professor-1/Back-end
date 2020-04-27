546e7db4004da3db7afd1410fc3fc3f291c53847
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-27 14:29:24 -0400

    added in initial helper function in router model

---
+++ router/router-model.js
@@ -0,0 +1,77 @@
+//import the database
+const db = require('../data/config');
+
+//helper functions
+
+//add user
+function addUser(user){
+    return db('users')
+        .insert(user, 'id')
+        .then(([id]) => {
+            return findUser(id)
+        })
+}
+
+//find user by their id
+function findUser(id) {
+    return db('users').where({id}).first();
+}
+
+//get a list of all the students under the user
+function getStudents(){
+    return db('students').where({professor_id});
+}
+
+//get student by their id
+function findStudent(id) {
+    return db('students').where({id}).first()
+}
+
+//add new student
+function addStudent(student){
+    return db('students')
+        .insert(student, 'id')
+        .then(([id]) => {
+            return findStudent(id)
+        });
+}
+
+//add new project to student object
+function addProject(project, student_id){
+    return db('projects')
+        .insert({...project, student_id})
+        .then(([id]) => {
+            return findProject(id);
+        })
+}
+
+//find the project by its id
+function findProject(id) {
+    return db('projects').where({id}).first();
+}
+
+//add a new message to the professor dashboard
+function addMessage(message){
+    return db('messages')
+        .insert({...message, user_id})
+        .then(([id]) => {
+            return findMessage(id);
+        });
+}
+
+//find the message by its id
+function findMessage(id) {
+    return db('messages').where({id}).first();
+}
+
+module.exports = {
+    findUser,
+    findStudent,
+    findProject,
+    findMessage,
+    addUser,
+    addStudent,
+    addProject,
+    addMessage,
+    getStudents
+}
