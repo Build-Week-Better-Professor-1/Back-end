bb8ea14e69226cb6b8a192188a9054d887623fb2
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-28 11:16:19 -0400

    fixed typo

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const auth = require("./auth.js");
 const studentRouter = require('../router/students/student-router.js');
 const userRouter = require('../router/users/users-router.js');
 const messagesRouter = require('../router/messages/messages-router.js');
-const projectsRouter = require('../router/messages/projects-router.js');
+const projectsRouter = require('../router/projects/projects-router.js');
 
 const server = express();
 
@@ -17,7 +17,7 @@ server.get("/api", (req, res) => {
 server.use("/api/auth", auth.router);
 server.use('/api/users', auth.validateToken, userRouter);
 server.use('/api/students', auth.validateToken, studentRouter);
-server.use('/api/messages', messageRouter);
+server.use('/api/messages', messagesRouter);
 server.use('/api/projects', projectsRouter);
 
 module.exports = server;
