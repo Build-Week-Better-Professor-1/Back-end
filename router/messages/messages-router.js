44e8e9f1ec4f4bbb09d9e61e85d3c23b77c76fb0
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-27 21:22:42 -0400

    edited some typos

--- router/messages/messages-router.js
+++ router/messages/messages-router.js
@@ -14,24 +14,24 @@ const router = express.Router();
 router.get('/', (req, res) => {
     Messages.getMessages()
         .then(appMessages => {
-            res.status(201).json({errorMessage: 'Rendering message list', appMessages})
+            res.status(201).json({ errorMessage: 'Rendering message list', appMessages })
         })
         .catch(err => {
-            res.status(500).json({errorMessage: 'Server error, could not render message list', err})
+            res.status(500).json({ errorMessage: 'Server error, could not render message list', err })
         })
 })
 
 router.get('/:id', (req, res) => {
     Messages.findMessage(id)
         .then(appMessage => {
-            if(!appMessage) {
-                res.status(404).json({errorMessage: 'Could not find the message with set id, please try again'})
+            if (!appMessage) {
+                res.status(404).json({ errorMessage: 'Could not find the message with set id, please try again' })
             } else {
-                res.status(201).json({message: 'Pulling up message information', appMessage})
+                res.status(201).json({ message: 'Pulling up message information', appMessage })
             }
         })
         .catch(err => {
-            res.status(500).json({errorMessage: "Server error, could not find message", err})
+            res.status(500).json({ errorMessage: "Server error, could not find message", err })
         })
 })
 
@@ -40,12 +40,12 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     const messageData = req.body;
 
-    Messages.addMessage({...messageData, user_id: req.token.id})
+    Messages.addMessage({ ...messageData, user_id: req.token.id })
         .then(newMessage => {
-            res.status(201).json({message: 'Message successfully added.', newMessage})
+            res.status(201).json({ message: 'Message successfully added.', newMessage })
         })
         .catch(err => {
-            res.status(500).json({errorMessage: 'Server error, could not add message'})
+            res.status(500).json({ errorMessage: 'Server error, could not add message' })
         })
 })
 
@@ -53,39 +53,39 @@ router.post('/', (req, res) => {
 //UPDATE
 //edit message
 router.put('/:id', (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const changes = req.body;
 
     Messages.findMessage(id)
         .then(appMessage => {
-            if(!appMessage) {
-                res.status(404).json({errorMessage: 'Could not update message with set id, please try again.'})
+            if (!appMessage) {
+                res.status(404).json({ errorMessage: 'Could not update message with set id, please try again.' })
             } else {
                 Messages.editMessage(changes, id)
-                .then(updated => {
-                    res.status(201).json({message: 'Message successfully updated', updated})
-                })
+                    .then(updated => {
+                        res.status(201).json({ message: 'Message successfully updated', updated })
+                    })
             }
         })
         .catch(err => {
-            res.status(500).json({errorMessage: 'Server error, could not update message', err})
+            res.status(500).json({ errorMessage: 'Server error, could not update message', err })
         })
 })
 
 
 //DELETE
 router.get('/', (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     Messages.deleteMessage(id)
         .then(deleted => {
-            if(!deleted) {
-                res.status(404).json({errorMessage: 'Could not delete message with set id, please try again.'})
+            if (!deleted) {
+                res.status(404).json({ errorMessage: 'Could not delete message with set id, please try again.' })
             } else {
-                res.status(201).json({message: 'Message successfully deleted', deleted})
+                res.status(201).json({ message: 'Message successfully deleted', deleted })
             }
         })
         .catch(err => {
-            res.status(500).json({errorMessage: 'Server error, could not delete message.'})
+            res.status(500).json({ errorMessage: 'Server error, could not delete message.' })
         })
 })
