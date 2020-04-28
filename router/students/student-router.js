de9b998c854e4d2d340da9e9b09dd24b4f70b1a5
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-28 15:39:37 -0600

    Fix failing test cases

--- router/students/student-router.js
+++ router/students/student-router.js
@@ -1,8 +1,8 @@
 //import express objects
-const express = require('express');
+const express = require("express");
 
 //setup Student Object
-const Students = require('./student-models');
+const Students = require("./student-models");
 
 //set up router object
 const router = express.Router();
@@ -11,106 +11,154 @@ const router = express.Router();
 
 //READ
 //get all students
-router.get('/', (req, res) => {
-    Students.getStudents(req.token.id)
-        .then(students => {
-            res.status(201).json({message: 'Rendering student list: ', students})
-        })
-        .catch(err => {
-            res.status(500).json({errorMessage: 'Server error, could not render student list. Contact backend.', err})
-        })
-})
+router.get("/", (req, res) => {
+  Students.getStudents(req.token.id)
+    .then((students) => {
+      res.status(200).json({ message: "Rendering student list: ", students });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage:
+          "Server error, could not render student list. Contact backend.",
+        err,
+      });
+    });
+});
 
 //get student by id
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
-    
-    Students.findStudent(id)
-        .then(student => {
-            if (!student){
-                res.status(404).json({errorMessage: 'Could not find student with set id.'})
-            } else {
-                res.status(201).json({message: 'Pulling up student information: ', student})
-            }
-        })
-        .catch(err => {
-            res.status(500).json({errorMessage: "Server error, could not find student id. Contact backend.", err})
-        })
-})
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Students.findStudent(id)
+    .then((student) => {
+      if (!student) {
+        res
+          .status(404)
+          .json({ errorMessage: "Could not find student with set id." });
+      } else {
+        res
+          .status(200)
+          .json({ message: "Pulling up student information: ", student });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage:
+          "Server error, could not find student id. Contact backend.",
+        err,
+      });
+    });
+});
 
 //get project by student id
-router.get('/:id/projects', (req, res) => {
-    const {id} = req.params;
+router.get("/:id/projects", (req, res) => {
+  const { id } = req.params;
 
-    Students.getProjectList(id)
-        .then(projects => {
-            res.status(201).json({message: 'Rendering Student Project List: ', projects})
-        })
-        .catch(err => {
-            res.status(500).json({errorMessage: 'Server error, could not render the specific student project list', err})
-        })
-})
+  Students.getProjectList(id)
+    .then((projects) => {
+      res
+        .status(200)
+        .json({ message: "Rendering Student Project List: ", projects });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage:
+          "Server error, could not render the specific student project list",
+        err,
+      });
+    });
+});
 
 //get project by student
 
-
 //CREATE
 //add student
-router.post('/', (req, res) => {
-    const studentData = req.body;
+router.post("/", validateStudentObj, (req, res) => {
+  const studentData = req.body;
 
-    Students.addStudent({...studentData, professor_id: req.token.id})
-        .then(student => {
-            res.status(201).json({message: 'Student successfully added', student})
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({message: 'Server error, student not added.', err})
-        })
-})
+  Students.addStudent({ ...studentData, professor_id: req.token.id })
+    .then((student) => {
+      res.status(201).json({ message: "Student successfully added", student });
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "Server error, student not added.", err });
+    });
+});
 
 //add project to student object
 
-
 //UPDATE
 //edit student
-router.put('/:id', (req, res) => {
-    const {id} = req.params;
-    const changes = req.body;
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
 
-    Students.findStudent(id)
-        .then(student => {
-            if(!student) {
-                res.status(404).json({errorMessage: "Unable to edit student; set id not found."})
-            } else {
-                Students.editStudent(changes, id)
-                    .then(updated => {
-                        res.status(200).json({message: "Student information updated.", updated});
-                    })
-            }
-        })
-        .catch(err => {
-            res.status(500).json({errorMessage: 'Server errror, unable to update student', err})
-        })
-})
-
+  Students.findStudent(id)
+    .then((student) => {
+      if (!student) {
+        res
+          .status(404)
+          .json({ errorMessage: "Unable to edit student; set id not found." });
+      } else {
+        Students.editStudent(changes, id).then((updated) => {
+          res
+            .status(200)
+            .json({ message: "Student information updated.", updated });
+        });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ errorMessage: "Server errror, unable to update student", err });
+    });
+});
 
 //DELETE
 //delete student
-router.delete('/:id', (req, res) => {
-  const {id} = req.params;
-  
+router.delete("/:id", (req, res) => {
+  const { id } = req.param;
+
   Students.deleteStudent(id)
-    .then(deleted => {
-        if(!deleted) {
-            res.status(404).json({errorMessage: 'Unable to delete student; set id not found.'})
-        } else {
-            res.status(201).json({message: 'Student successfully deleted.', deleted})
-        }
+    .then((deleted) => {
+      if (!deleted) {
+        res.status(404).json({
+          errorMessage: "Unable to delete student; set id not found.",
+        });
+      } else {
+        res
+          .status(200)
+          .json({ message: "Student successfully deleted.", deleted });
+      }
     })
-    .catch(err => {
-        res.status(500).json({errorMessage: 'Server errror, unable to delete student', err})
-    })
-})
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ errorMessage: "Server errror, unable to delete student", err });
+    });
+});
+
+function validateStudentObj(req, res, next) {
+  if (req.body == undefined) {
+    res.status(400).json({ errorMessage: "Invalid body" });
+    return;
+  }
+
+  const missingFields = [];
+  if (req.body.name === undefined) missingFields.push("name");
+  if (req.body.email === undefined) missingFields.push("email");
+
+  if (missingFields.length > 0) {
+    res.status(400).json({
+      errorMessage: `The following required fields are missing: ${missingFields}`,
+    });
+    return;
+  }
+
+  next();
+}
 
 module.exports = router;
