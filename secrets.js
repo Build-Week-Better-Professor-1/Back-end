664d6c6f7366f8ce5f1c07c4d9949b7f6616dff2
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-29 15:26:29 -0600

    Parse HASH_ROUNDS into an integer

--- secrets.js
+++ secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  hashRounds: process.env.HASH_ROUNDS || 8,
+  hashRounds: parseInt(process.env.HASH_ROUNDS) || 8,
   jwtSecret: process.env.JWT_SECRET || "not all that is gold glitters",
 };
