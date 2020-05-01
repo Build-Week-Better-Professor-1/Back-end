754516a554315c224b301a9ac0afa288225e2423
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-05-01 10:14:33 -0600

    Make register return 409 for duplicate email

--- api/auth.js
+++ api/auth.js
@@ -13,59 +13,64 @@ module.exports = {
   validateToken,
 };
 
-router.post("/register", validateUserObj(true), (req, res) => {
+router.post(
+  "/register",
+  getUserByEmailOpt,
+  validateUserObj(true),
+  (req, res) => {
+    if (req.user !== undefined) {
+      res
+        .status(409)
+        .json({ errorMessage: "User with that email already exists" });
+      return;
+    }
+
+    const creds = {
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, secrets.hashRounds),
+    };
+    model
+      .addUser(creds)
+      .then((user) => {
+        res.status(201).json({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          token: generateToken(user),
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ errorMessage: "Failed to add user to database" });
+      });
+  }
+);
+
+router.post("/login", getUserByEmailOpt, validateUserObj(false), (req, res) => {
   const creds = {
-    name: req.body.name,
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, secrets.hashRounds),
   };
-  model
-    .addUser(creds)
-    .then((user) => {
-      res.status(201).json({
+
+  const user = req.user;
+
+  if (user) {
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      res.status(200).json({
         id: user.id,
         name: user.name,
         email: user.email,
         token: generateToken(user),
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ errorMessage: "Failed to add user to database" });
-    });
-});
-
-router.post("/login", validateUserObj(false), (req, res) => {
-  const creds = {
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, secrets.hashRounds),
-  };
-  model
-    .findUserByEmail(req.body.email)
-    .then((user) => {
-      if (user) {
-        if (bcrypt.compareSync(req.body.password, user.password)) {
-          res.status(200).json({
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user),
-          });
-        } else {
-          res.status(401).json({ errorMessage: "Password is incorrect" });
-        }
-      } else {
-        res
-          .status(404)
-          .json({ errorMessage: "No user with the specified email" });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        errorMessage: "Failed to retrieve list of users from database",
-      });
-    });
+    } else {
+      res.status(401).json({ errorMessage: "Password is incorrect" });
+    }
+  } else {
+    res.status(404).json({ errorMessage: "No user with the specified email" });
+  }
 });
 
 function validateUserObj(checkForName) {
@@ -83,6 +88,21 @@ function validateUserObj(checkForName) {
 
     next();
   };
+}
+
+function getUserByEmailOpt(req, res, next) {
+  model
+    .findUserByEmail(req.body.email)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ errorMessage: "Failed to retrieve students data" });
+    });
 }
 
 function generateToken(user) {
